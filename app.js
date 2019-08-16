8b66d72928bfbad20ec243889223d5fafe01c6ec
AleksanderWoltosz <aleksander.woltosz@gmail.com>
2019-08-16 18:28:47 +0200

    qc update

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const formEpph1 = document.querySelector('.formEpph1');
 const formEpph2 = document.querySelector('.formEpph2');
 const formEpph3 = document.querySelector('.formEpph3');
 const formQc1 = document.querySelector('.formQc1');
-const formQc2 = document.querySelector('.formQc2');
-const formQc3 = document.querySelector('.formQc3');
 const formCsat = document.querySelector('.formCsat');
 
 const reset = document.querySelector('.reset');
@@ -18,10 +16,8 @@ const agentName = document.querySelector('.agentName');
 const epph1 = document.querySelector('.epph1');
 const epph2 = document.querySelector('.epph2');
 const epph3 = document.querySelector('.epph3');
-const qc1 = document.querySelector('.qc1')
-const qc2 = document.querySelector('.qc2')
-const qc3 = document.querySelector('.qc3')
-const csat = document.querySelector('.csat')
+const qc1 = document.querySelector('.qc1');
+const csat = document.querySelector('.csat');
 
 formTeamName.addEventListener('change', (e)=>{
     console.log(e.target.value)
@@ -49,13 +45,6 @@ formQc1.addEventListener('change', (e)=>{
     qc1.textContent = e.target.value;
 })
 
-formQc2.addEventListener('change', (e)=>{
-    qc2.textContent = e.target.value;
-})
-
-formQc3.addEventListener('change', (e)=>{
-    qc3.textContent = e.target.value;
-})
 
 formCsat.addEventListener('change', (e)=>{
     csat.textContent = e.target.value;
